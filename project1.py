b752bc584e9275551b46d7faf02cba848cb03057
Carlos Araki <carlos.araki@ws-solutions.com>
2023-01-03 21:37:03 -0300

    adding env

---
+++ project1.py
@@ -0,0 +1,28 @@
+import sklearn
+import neat
+import pygame
+import os, cv2
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+from torchvision import transforms
+import numpy as np
+import requests
+import random 
+#import mtcnn
+from torch import nn
+import numpy
+# scipy.special for the sigmoid function expit()
+#importing the os module
+# para pegar path 
+import os
+# helper to load data from PNG image files
+import imageio
+# glob helps select multiple files using patterns
+import glob
+#para pegar tempo de criacao da rede
+import datetime
+
+import scipy.special
+# library for plotting arrays
+import matplotlib.pyplot
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Env basic 
+
+`
+    source v_env/bin/activate
+    deactivate
+`
